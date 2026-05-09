e8e8731b8d8d6ac93a1caa49f2d3c6ce59369c9c
jburarc1311 <jburarc1311@g.educaand.es>
2026-05-09 20:52:20 +0200

    Conditionally load dotenv configuration
    Load environment variables conditionally based on NODE_ENV.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import dotenv from "dotenv";
-dotenv.config();
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
+console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
 import express from "express";
 import cookieParser from "cookie-parser";
