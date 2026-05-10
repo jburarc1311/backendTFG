7e45be20d8704783cf80654e81638fd53d1c4f83
jburarc1311 <jburarc1311@g.educaand.es>
2026-05-10 13:18:31 +0200

    Change default PORT from 3000 to 8080

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import {config} from 'dotenv'
 config(); //leer las variables de entorno
  
 
-export const PORT=process.env.PORT || 3000; 
+export const PORT=process.env.PORT || 8080; 
 export const URI=process.env.URI
 export const SECRET_KEY=process.env.SECRET_KEY
 export const REFRESH_SECRET_KEY=process.env.REFRESH_SECRET_KEY
