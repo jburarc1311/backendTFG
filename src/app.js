ce9db1d5c9b6ecc802d61385369d52ad497ff764
jburarc1311 <jburarc1311@g.educaand.es>
2026-05-10 13:20:24 +0200

    Update server listen address and log message

--- src/app.js
+++ src/app.js
@@ -90,8 +90,8 @@ app.use((req, res) => {
 // Iniciar servidor
 conexionBD()
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Servidor corriendo en el puerto${PORT}`);
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Servidor corriendo en el puerto ${PORT}`);
     });
   })
   .catch((err) => {
