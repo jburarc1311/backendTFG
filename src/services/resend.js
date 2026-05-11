ae495f529fc888c1766f3de31b1cf38b4349d3bc
José Antonio Burguillos Arcos <jaburguillos@deuser.es>
2026-05-11 13:36:14 +0200

    Agregar manejo de errores en la función 'enviarEmailContacto' para mejorar la robustez del envío de correos

--- src/services/resend.js
+++ src/services/resend.js
@@ -3,17 +3,28 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const enviarEmailContacto = async ({ nombre, motivo, mensaje }) => {
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "joseab078@gmail.com",
-    subject: `Contacto: ${motivo}`,
-    html: `
-      <h2>Nuevo mensaje de contacto</h2>
-      <p><b>Nombre:</b> ${nombre}</p>
-      <p><b>Motivo:</b> ${motivo}</p>
-      <p><b>Mensaje:</b> ${mensaje}</p>
-    `,
-  });
+  try {
+    const response = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: "joseab078@gmail.com",
+      subject: `Contacto: ${motivo}`,
+      html: `
+        <h2>Nuevo mensaje de contacto</h2>
+        <p><b>Nombre:</b> ${nombre}</p>
+        <p><b>Motivo:</b> ${motivo}</p>
+        <p><b>Mensaje:</b> ${mensaje}</p>
+      `,
+    });
+
+    console.log("RESEND RESPONSE:", response);
+
+    return response;
+
+  } catch (error) {
+    console.error("ERROR RESEND:", error);
+
+    throw error;
+  }
 };
 
 
