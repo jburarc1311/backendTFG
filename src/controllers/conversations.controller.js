cc23d7065d3c895b1fba031bec08ed29f96a6b15
José Antonio Burguillos Arcos <jaburguillos@deuser.es>
2026-05-20 11:28:27 +0200

    Agregar comentario explicativo en la búsqueda de conversaciones para clarificar la lógica de participantes

--- src/controllers/conversations.controller.js
+++ src/controllers/conversations.controller.js
@@ -27,7 +27,7 @@ export const createOrGetConversation = async (req, res) => {
 
     // Buscar conversación que contenga solo estos dos participantes
     const existing = await Conversation.findOne({
-      participants: { $all: participants},
+      participants: { $all: participants}, // es para asegurarnos que ambos participantes estén en la conversación
     });
 
     if (existing) return res.json(existing); // Si ya existe, devuelve la conversación
